9c3abc1ccb9ef86eb83157bc396002ce40eee66b
hugo <64664896+hugoduchene@users.noreply.github.com>
2022-11-11 18:56:35 +0100

    WIP

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ export const router = {
         success: "/payment/success",
     },
     offer: {
+        funnelOffer0: "/offer/0",
         funnelOffer1: "/offer/1",
         funnelOffer2: "/offer/2",
         funnelOfferSupplement: "/offer/supplement/:idSupplement"
--- src/utils/asset/offers.ts
+++ src/utils/asset/offers.ts
@@ -1,8 +1,11 @@
+import { router } from "../../router";
+
 export interface IOfferData {
     version: string;
     price: number;
     items: Array<string>;
     uid: string;
+    url: string;
 }
 
 export const offersData: Array<IOfferData> = [
@@ -11,34 +14,39 @@ export const offersData: Array<IOfferData> = [
       version: "freeOffer",
       price: 0,
       // eslint-disable-next-line max-len
-      items: ["1offer1text", "1offer2text", "1offer3text", "1offer4text", "1offer5text", "1offer6text", "1offer7text"]
+      items: ["1offer1text", "1offer2text", "1offer3text", "1offer4text", "1offer5text", "1offer6text", "1offer7text"],
+      url: router.offer.funnelOffer0, 
     },
     {
       uid: "3f2d2c28-dd3e-482a-9dd7-bab8dae2fe76",
       version: "onlineOffer",
       price: 99,
       // eslint-disable-next-line max-len
-      items: ["1offer1text", "1offer2text", "1offer3text", "1offer5text", "2offer6text", "2offer8text", "2offer9text", "2offer10text", "2offer11text", "2offer12text", "2offer13text"]
+      items: ["1offer1text", "1offer2text", "1offer3text", "1offer5text", "2offer6text", "2offer8text", "2offer9text", "2offer10text", "2offer11text", "2offer12text", "2offer13text"],
+      url: router.offer.funnelOffer1,
     },
     {
       uid: "6804e2f3-e38b-4eba-b9d7-5335cbb3a342",
       version: "versionOffer3",
       price: 199,
       // eslint-disable-next-line max-len
-      items: ["3offer13text", "3offer14text", "3offer15text", "3offer16text", "3offer17text", "3offer18text"]
+      items: ["3offer13text", "3offer14text", "3offer15text", "3offer16text", "3offer17text", "3offer18text"],
+      url: router.offer.funnelOffer2,
     },
     {
       uid: "ed8b84db-7bd7-4efc-bc54-c6ef4f55df0b",
       version: "versionOffer4",
       price: 129,
       // eslint-disable-next-line max-len
-      items: ["4offer1text", "4offer2text", "4offer3text", "4offer4text", "4offer5text", "4offer6text"]
+      items: ["4offer1text", "4offer2text", "4offer3text", "4offer4text", "4offer5text", "4offer6text"],
+      url: router.offer.funnelOfferSupplement,
     },
     {
       uid: "085958e5-4219-410e-b1d3-25c1c40e9208",
       version: "versionOffer5",
       price: 149,
       // eslint-disable-next-line max-len
-      items: ["5offer1text", "5offer2text", "5offer3text", "5offer4text", "5offer5text", "5offer6text"]
+      items: ["5offer1text", "5offer2text", "5offer3text", "5offer4text", "5offer5text", "5offer6text"],
+      url: router.offer.funnelOfferSupplement,
     }
   ];
